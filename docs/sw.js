192fdd08ed4b69cad5ebed0b0742111dbe58a7b7
programwebsiteW <295442317+programwebsiteW@users.noreply.github.com>
2026-08-17 18:22:01 -0500

    Escurece tela de entrada

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE='lu-fashion-hair-v2';
-const APP_SHELL=['/','/css/style.css?v=20260817-mobile2','/js/api.js?v=20260817-mobile2','/js/app.js?v=20260817-mobile2','/assets/logo-lu-fashion-hair.png','/assets/app-icon-192.png','/assets/app-icon-512.png','/manifest.webmanifest'];
+const CACHE='lu-fashion-hair-v3';
+const APP_SHELL=['/','/css/style.css?v=20260817-mobile3','/js/api.js?v=20260817-mobile2','/js/app.js?v=20260817-mobile2','/assets/logo-lu-fashion-hair.png','/assets/app-icon-192.png','/assets/app-icon-512.png','/manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
